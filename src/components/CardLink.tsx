994aa555c856759c0234409e36f07eb9a802d6fb
DrxwDev <drxwdev@outlook.es>
2025-07-13 18:00:13 -0400

    Adding Hover effect

--- src/components/CardLink.tsx
+++ src/components/CardLink.tsx
@@ -10,10 +10,13 @@ export default function CardLink({ id, text }: LinkProps) {
   return (
     <div
       id={`${id}`}
-      className="bg-[var(--customGrey700)] w-full py-3 rounded-lg"
+      className="bg-[var(--customGrey700)] w-full py-3 rounded-lg hover:cursor-pointer transition-all duration-200"
     >
       <Link href="#">
-        <Text textStyles="text-white font-bold text-[.9rem]" text={text} />
+        <Text
+          textStyles="text-white font-bold hover:text-[var(--customGreen)] text-[.9rem] hover:text[var(--customGreen)] transition-all duration-200"
+          text={text}
+        />
       </Link>
     </div>
   );
